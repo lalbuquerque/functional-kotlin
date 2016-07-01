0fce43c99105164ed88a643fe4fa8a29d7ea2818
Lucas Albuquerque <lucas.albuquerque12@gmail.com>
2016-07-01 10:31:28 -0300

    :man: Trying out locationToCoordinates function

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,9 @@
+import data.Location
 import java.util.*
 
 /**
- * Created by lalbuquerque on 6/30/16.
- */
+* Created by lalbuquerque on 6/30/16.
+*/
 
 fun main(args: Array<String>) {
     val list = ArrayList<Int>()
@@ -11,13 +12,17 @@ fun main(args: Array<String>) {
     list.add(3)
     list.add(4)
 
-    println(plus1(list))
-}
+    println("plus1 -> " + plus1(list))
 
-/**
- * Adds 1 to each element of a given List<Int>
- */
-fun plus1(intList: List<Int>) : List<Int> {
-    if (intList.size == 0) return emptyList()
-    return intList.map { it + 1 }
+    // ----------------------------
+
+    val tempList = ArrayList<Int>()
+    tempList.add(75)
+    tempList.add(75)
+    tempList.add(75)
+    tempList.add(75)
+    tempList.add(75)
+
+    val loc = Location("Funky Town", 1000000,tempList)
+    println("locationToCoordinates -> " + locationToCoordinates(loc))
 }
